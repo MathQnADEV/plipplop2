96eb0fa34f7a1cb1746818fb276d7dbb07a795ea
MathQnADEV <mathqna.project@gmail.com>
2024-02-06 21:31:42 +0800

    fix: issue maxcall

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -2,7 +2,7 @@ import axios from "./axios";
 
 export const getAnimeResponse = async (resource, query) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
   );
   const anime = await response.json();
   return anime;
@@ -10,15 +10,15 @@ export const getAnimeResponse = async (resource, query) => {
 
 export const getFanartResponse = async (resource) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_ADMIN_URL}/${resource}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_ADMIN_URL}/${resource}`
   );
   const fanart = await response.json();
   return fanart;
 };
 
 export const getNestedAnimeResponse = async (resource, objectProperty) => {
-    const response = await getAnimeResponse(resource);
-    return response.data.flatMap(item => item[objectProperty]);
+  const response = await getAnimeResponse(resource);
+  return response.data.flatMap((item) => item[objectProperty]);
 };
 
 // randomize data
@@ -34,20 +34,20 @@ export const reproduce = (data, gap) => {
 
 export const getMangaResponse = async (resource, query) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
   );
   const manga = await response.json();
   return manga;
 };
 
 export const getNestedMangaResponse = async (resource, objectProperty) => {
-    const response = await getMangaResponse(resource);
-    return await response.data.flatMap(item => item[objectProperty]);
+  const response = await getMangaResponse(resource);
+  return await response.data.flatMap((item) => item[objectProperty]);
 };
 
 export const getDataCheckUser = async (data) => {
-    const response = await axios.post("/userPlipPlopCheck", {
-      email: data,
-    });
-    return await response.data.data;
+  const response = await axios.post("/userPlipPlopCheck", {
+    email: data,
+  });
+  return await response.data.data;
 };
